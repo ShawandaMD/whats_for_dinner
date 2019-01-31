57bbc54f9bc1b42cfc7a19c70675d8854883820d
github username <github email address>
2019-01-31 18:53:20 -0500

    Added empty methods to class.

--- lib/whats_for_dinner/meals.rb
+++ lib/whats_for_dinner/meals.rb
@@ -2,4 +2,21 @@ class WhatsForDinner::Meals
   attributes :title, :link, :subtitle, :time, :difficulty,
    :spice, :description, :chef, :calories
 
+   def initialize()
+
+   end
+
+   def self.create_by_array(array_of_meals) #self because it's creating an instance
+
+   end
+
+   def add_meal_details(hash)
+
+   end
+
+   def self.all
+
+   end
+
+
 end
