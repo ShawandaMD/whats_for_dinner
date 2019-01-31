ce25eb9a2cf103dc48b9265c32489e077f89bda6
github username <github email address>
2019-01-31 17:36:54 -0500

    WhatsForDinner::Scraper.scrap_meal_details method to scrape the details of the meal url.

--- lib/whats_for_dinner/scraper.rb
+++ lib/whats_for_dinner/scraper.rb
@@ -1,30 +1,29 @@
 class WhatsForDinner::Scraper
-#WhatsForDinner::Scraper.scrap_meals
 
   def self.scrap_meals #make this an argument for url for doc
-    #html = File.read("")
     doc = Nokogiri::HTML(open("https://www.homechef.com/our-menus/25-feb-2019/standard"))
     meals = []
 
     doc.css("div.cards article#meal").each  do |meal|
       title = meal.css("h6.text--body").text
-      link = meal.css("a.card__panel").attribute("href").value
+      link = "https://www.homechef.com" + (meal.css("a.card__panel").attribute("href").value)
       meals << {title: title, link: link }
     end
     meals
   end
 
-  def self.scrap_meal_details
-    #Second level
-    #title in link: level.css("header.meal__header.order--1.group span").text
-    #time to cook: level.css("p.margin--1bottom span.is--block--bpDown2")[1].text
-    #spice level: level.css("span.is--block--bpDown2 span.is--block--bpDown2.margin--1bottom--bpDown2")[1].text
-    #difficulty: level.css("span.is--block--bpDown2 span.is--block--bpDown2.margin--1bottom--bpDown2")[0].text
-    #calories: level.css("li.margin--2bottom.size--xs--bpDown2")[0].text.split("\n")[-1] #537
-    #chef: level.css("h4.h--serif.size--xs.size--sm--bpDown2.no--margin").text
-    #description: level.css("div.flexGrid__item.padding--6left--bpUp3.margin--3top--bpDown2.size--xs--bpDown2").text
+  def self.scrap_meal_details(url)
+    details = Nokogiri::HTML(open(url)) #"https://www.homechef.com/meals/roasted-red-pepper-chicken"
 
+    meal = {}
+      meal[:subtitle] = details.css("header.meal__header.order--1.group span h2").text
+      meal[:time] = details.css("p.margin--1bottom span.is--block--bpDown2")[1].text
+      meal[:spice] = details.css("span.is--block--bpDown2 span.is--block--bpDown2.margin--1bottom--bpDown2")[1].text
+      meal[:difficulty] = details.css("span.is--block--bpDown2 span.is--block--bpDown2.margin--1bottom--bpDown2")[0].text
+      meal[:calories] = details.css("li.margin--2bottom.size--xs--bpDown2")[0].text.split("\n")[-1]
+      meal[:chef] = details.css("h4.h--serif.size--xs.size--sm--bpDown2.no--margin").text
+      meal[:description] = details.css("div.flexGrid__item.padding--6left--bpUp3.margin--3top--bpDown2.size--xs--bpDown2 p").text
+    meal
   end
-
-
+  #Fruit Basket is going to give me nil!!!
 end
